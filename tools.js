3809e86f05175de5e8456a9728d84f43e6431db5
MinjiK <km.minjikim@gmail.com>
2022-08-23 10:36:18 +0200

    fixed tools for correct testing

--- tools.js
+++ tools.js
@@ -1,5 +1,5 @@
 module.exports = function() { 
-    this.sum = function(a,b) { return a-b };
+    this.sum = function(a,b) { return a+b };
     //this.multiply = function(a,b) { return a*b };
     //more
 }
